5cea665cc7018091df6a7e4343925c29b04474d8
Artem Halushka <ahalushka.laba@solvd.com>
2023-10-25 22:55:06 -0700

    added implementation for selection sorting method

---
+++ src/Selection.java
@@ -0,0 +1,34 @@
+public class Selection {
+
+    //Function for sorting using selection method
+    public void sort(int[] array) {
+
+        // First loop for iterating through array
+        for(int i = 0; i<array.length-1; i++) {
+            int currentMin = i;
+
+            // Second loop for finding minimal number in array
+            for(int j = i+1; j< array.length; j++) {
+                if (array[j] < array[currentMin]) {
+                    currentMin = j;
+                }
+            }
+
+            // Swap Current iterable element with minimal number
+            int temp = array[currentMin];
+            array[currentMin] = array[i];
+            array[i] = temp;
+        }
+    }
+
+    // Main function
+    public static void main(String[] args) {
+        int array[] = {99, 231, 53, 1, 4};
+        Selection selectionSort = new Selection();
+        //Function call
+        selectionSort.sort(array);
+        for (int num : array) {
+            System.out.print(num + " ");
+        }
+    }
+}
